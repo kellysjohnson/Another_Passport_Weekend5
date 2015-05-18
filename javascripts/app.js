66d4dad5f6af4290e37c0803e0d29d91d6c23651
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-05-18 16:34:33 -0500

    Includes username and information blocks with basic styling

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,9 +2,13 @@
 
 // Append Users
 function appendData(userData) {
+    var containsData = '<div class="holder" "small">';
+
     for (var j = 0; j < userData.length; j++) {
-        $('.appendhere').append(userData[j].username + "<br>");
+        containsData += '<div> username: ' + userData[j].username + '</div><div> firstname: ' + userData[j].firstname + '</div><div> lastname: ' + userData[j].lastname + '</div><div> email: ' + userData[j].email + '</div><br/>'
     }
+    $('.appendhere').append(containsData + '</div>');
+    console.log(containsData);
 }
 
 // AJAX call to pull user Data
@@ -12,7 +16,7 @@ function getUsers(data) {
     $.ajax({
         type: 'GET',
         url: '/users/getData',                              //  where are my USERS??????   Answer: you have to look for them in /getData.
-        datatype: 'application/json',                       //  This is after creating a route
+        datatype: 'json',                       //  This is AFTER creating a route *app.get('/getData')* where the Users.find method is performed.
         success: function (response) {
             console.log("Some Data", response);
             appendData(response);
